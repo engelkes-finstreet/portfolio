1972356e41937dd04b8eb9aab7529e40ba84b801
Patrick Engelkes <patrick.engelkes@finstreet.de>
2023-06-28 22:43:14 +0200

    Fix article layout.

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -12,7 +12,24 @@ function Article({ article }) {
         <Card.Title href={`/articles/${article.slug}`}>
           {article.title}
         </Card.Title>
+        <Card.Eyebrow
+          as="time"
+          dateTime={article.date}
+          className="md:hidden"
+          decorate
+        >
+          {formatDate(article.date)}
+        </Card.Eyebrow>
+        <Card.Description>{article.description}</Card.Description>
+        <Card.Cta>Read article</Card.Cta>
       </Card>
+      <Card.Eyebrow
+        as="time"
+        dateTime={article.date}
+        className="mt-1 hidden md:block"
+      >
+        {formatDate(article.date)}
+      </Card.Eyebrow>
     </article>
   );
 }
