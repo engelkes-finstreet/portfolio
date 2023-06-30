97b7ff5e04f32ba5eb475636edaaaac5d923d8f5
Patrick Engelkes <patrick.engelkes@finstreet.de>
2023-06-30 15:10:51 +0200

    Light mode adjustment for Conversation component.

--- src/components/chatgpt-conversation/index.tsx
+++ src/components/chatgpt-conversation/index.tsx
@@ -17,12 +17,12 @@ function Conversation({ question, answer, defaultOpen }: Conversation) {
     <Disclosure defaultOpen={defaultOpen}>
       {({ open }) => (
         <>
-          <Disclosure.Button className="flex w-full justify-between rounded-lg bg-zinc-600 px-4 py-2 text-left text-sm font-medium text-zinc-50 hover:bg-zinc-500 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+          <Disclosure.Button className="flex w-full justify-between rounded-lg bg-zinc-300 px-4  py-2 text-left text-sm font-medium text-zinc-800 hover:bg-zinc-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75  dark:bg-zinc-600 dark:text-zinc-50 dark:hover:bg-zinc-500">
             <span>{question}</span>
             <ChevronUpIcon
               className={`${
                 open ? "rotate-180 transform" : ""
-              } h-5 w-5 text-blue-200`}
+              } h-5 w-5 text-zinc-800 dark:text-zinc-200`}
             />
           </Disclosure.Button>
           <Disclosure.Panel className="prose px-4 pb-2 dark:prose-invert prose-h2:mt-8">
@@ -38,7 +38,7 @@ export const ChatGptConversation = ({ conversations }: Props) => {
   return (
     <div className="w-full">
       <h3>Conversation with ChatGPT:</h3>
-      <div className="mx-auto w-full space-y-4 rounded-2xl bg-zinc-800 p-4">
+      <div className="mx-auto w-full space-y-4 rounded-2xl bg-zinc-100 p-4 dark:bg-zinc-800">
         {conversations.map((conversation) => (
           <Conversation
             key={conversation.question}
