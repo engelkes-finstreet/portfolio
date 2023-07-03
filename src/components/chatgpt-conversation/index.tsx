cf9d97258d4a4ff5462d00c9378b3ae175996fd4
Patrick Engelkes <patrick.engelkes@finstreet.de>
2023-07-03 11:23:58 +0200

    Improve conversation component.

--- src/components/chatgpt-conversation/index.tsx
+++ src/components/chatgpt-conversation/index.tsx
@@ -3,9 +3,8 @@ import Markdown from "marked-react";
 import { Disclosure } from "@headlessui/react";
 
 export type Conversation = {
-  question: string;
+  prompt: string;
   answer: string;
-  defaultOpen: boolean;
 };
 
 type Props = {
@@ -13,13 +12,13 @@ type Props = {
   title: string;
 };
 
-function Conversation({ question, answer, defaultOpen }: Conversation) {
+function Conversation({ prompt, answer }: Conversation) {
   return (
-    <Disclosure defaultOpen={defaultOpen}>
+    <Disclosure>
       {({ open }) => (
         <>
           <Disclosure.Button className="flex w-full justify-between rounded-lg bg-zinc-300 px-4  py-2 text-left text-sm font-medium text-zinc-800 hover:bg-zinc-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75  dark:bg-zinc-600 dark:text-zinc-50 dark:hover:bg-zinc-500">
-            <span>{question}</span>
+            <span>{prompt}</span>
             <ChevronUpIcon
               className={`${
                 open ? "rotate-180 transform" : ""
@@ -42,10 +41,9 @@ export const ChatGptConversation = ({ conversations, title }: Props) => {
       <div className="mx-auto w-full space-y-4 rounded-2xl bg-zinc-100 p-4 dark:bg-zinc-800">
         {conversations.map((conversation) => (
           <Conversation
-            key={conversation.question}
-            question={conversation.question}
+            key={conversation.prompt}
+            prompt={conversation.prompt}
             answer={conversation.answer}
-            defaultOpen={conversation.defaultOpen}
           />
         ))}
       </div>
