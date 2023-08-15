090f191f4f873c5162283006f829e416abff1900
Patrick Engelkes <patrick.engelkes@finstreet.de>
2023-08-15 16:03:27 +0200

    4th part of blog post.

--- src/components/chatgpt-conversation/index.tsx
+++ src/components/chatgpt-conversation/index.tsx
@@ -1,6 +1,7 @@
 import { ChevronUpIcon } from "@heroicons/react/20/solid";
 import Markdown from "marked-react";
 import { Disclosure } from "@headlessui/react";
+import { useState } from "react";
 
 export type Conversation = {
   prompt: string;
@@ -13,6 +14,9 @@ type Props = {
 };
 
 function Conversation({ prompt, answer }: Conversation) {
+  const shortAnswer = answer.substr(0, 100);
+  const needsMore = answer.length > 100;
+
   return (
     <Disclosure>
       {({ open }) => (
@@ -22,11 +26,26 @@ function Conversation({ prompt, answer }: Conversation) {
             <ChevronUpIcon
               className={`${
                 open ? "rotate-180 transform" : ""
-              } h-5 w-5 text-zinc-800 dark:text-zinc-200`}
+              } h-5  w-5 min-w-[20px] text-zinc-800 dark:text-zinc-200`}
             />
           </Disclosure.Button>
-          <Disclosure.Panel className="prose px-4 pb-2 dark:prose-invert prose-h2:mt-8">
-            <Markdown>{answer}</Markdown>
+          {!open && (
+            <div className={"p-4 text-zinc-300"}>
+              <Markdown>{open ? answer : `${shortAnswer}...`}</Markdown>
+              {needsMore && !open && (
+                <Disclosure.Button className={"mt-4 text-white underline"}>
+                  Show more
+                </Disclosure.Button>
+              )}
+            </div>
+          )}
+          <Disclosure.Panel className="px-4 pb-2">
+            <div className={"prose dark:prose-invert prose-h2:mt-8"}>
+              <Markdown>{answer}</Markdown>
+            </div>
+            <Disclosure.Button className={"text-white underline"}>
+              Show less
+            </Disclosure.Button>
           </Disclosure.Panel>
         </>
       )}
